3a4a32a53f4fc1df23aee5c7199fa2d5f637320e
Altansukh <Gold_sw06@yahoo.com>
2021-11-27 00:18:10 +0800

    voiceover data shinchille

--- schemas/schema.js
+++ schemas/schema.js
@@ -11,6 +11,86 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      title: "Шинэ дуу оруулах",
+      name: "post",
+      type: "document",
+      fields:[
+        {
+          title:'Аудио файлын нэр',
+          name:"AudioFile",
+          type:'file',
+        },
+        {
+          title:'Нэр',
+          name:"names",
+          type:'string',
+        },
+        {
+          title:'Нас',
+          name:"age",
+          type:'reference',
+          to:[{type:'age'}]
+        },
+        {
+          title:'Хүйс',
+          name:"Gender",
+          type:'reference',
+          to:[{type:'Gender'}]
+        },
+        {
+          title:'Хэл',
+          name:"publisher",
+          type:'reference',
+          to:[{type:'publisher'}]
+        },
+        {
+          title:'Туршлага',
+          name:"Level",
+          type:'string',
+        },
+        {
+          title:'Огно',
+          name:"date",
+          type:'datetime',
+        },
+      ]
+    },
+    {
+      title: "Хэл нэмэх",
+      name: "publisher",
+      type: "document",
+      fields:[
+        {
+          title:"Сонгогдох хэл",
+          name:"title",
+          type:"string",
+        },
+      ],
+    },
+    {
+      title: "Хүйс нэмэх",
+      name: "Gender",
+      type: "document",
+      fields:[
+        {
+          title:"Сонгогдох хүйс",
+          name:"title",
+          type:"string",
+        },
+      ],
+    },
+    {
+      title: "Нас нэмэх",
+      name: "age",
+      type: "document",
+      fields:[
+        {
+          title:"Сонгогдох нас",
+          name:"title",
+          type:"string",
+        },
+      ],
+    }
   ]),
 })
